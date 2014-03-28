92bca635cf939975e444abdb9638f0c0ad051f7f
Hack Reactor Students <accounts@hackreactor.com>
2014-03-27 19:55:43 -0700

    AZURE Y U DO DIS

--- app/collections/links.js
+++ app/collections/links.js
@@ -1,9 +1,21 @@
-// NOTE: this file is not needed when using MongoDB
-var db = require('../config');
-var Link = require('../models/link');
+// // NOTE: this file is not needed when using MongoDB
+// var db = require('../config');
+// var Link = require('../models/link');
 
-var Links = new db.Collection();
+// //mongoose
+// var Links = new db.Collection();
 
-Links.model = Link;
+// //mongoose
+// Links.model = Link;
 
-module.exports = Links;
+// module.exports = Links;
+
+// var Backbone = require('Backbone');
+// var Link = require("../models/link");
+
+// var Links = Backbone.Collection.extend({
+//   model: Link,
+//   mongooseModel: "linkModel",
+// });
+
+// exports = module.exports = Links;
